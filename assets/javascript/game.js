40053a0871439cd059693d94d59bb87173a822b6
FuzzyJones <donald.jones83@gmail.com>
2017-12-24 14:58:08 -0500

    some coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,33 @@
+
+// words to be selected randomly
 var wordList = [
-    "freddy",
+    "freddy krueger",
     "pinhead",
     "leatherface",
-    "hannibal",
+    "hannibal lecter",
     "pennywise",
-    "jason",
+    "jason voorhees",
     "candyman"
 ]
+
+// letters user can guess
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// set variables for the game
+var wins = 0;
+var currentWord = [];
+var guessLeft = 15;
+var lettersGuessed = [];
+var userGuess = [];
+
+// randomly selected word variable
+var computerGuess = wordList[Math.floor(Math.random() * wordList.length)];
+
+
+// functions of game
+document.onkeyup = function(event){
+    for (var i = 0; i < wordList.length; i++) {
+        currentWord [i] = "_";
+    }
+    console.log(computerGuess);
+}
