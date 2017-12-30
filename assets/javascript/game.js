3bfb0bf1a3adfda5e70f9439d94d25fdb167525a
FuzzyJones <donald.jones83@gmail.com>
2017-12-30 12:48:55 -0500

    javascript progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,15 @@
+// pseudo code
+// create variables of the game
+    // create random word generator
+// create functions to be called on in the game
+    // underscores
+// create a key up function for the user letter choices
+// push the results to the html
+// reset after win/lose
 
-// words to be selected randomly
+
+
+// words to be selected randomly by computers
 var wordList = [
     "freddy krueger",
     "pinhead",
@@ -18,19 +28,51 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 
 // set variables for the game
 var wins = 0;
+var loses = 0;
 var currentWord = [];
-var guessLeft = 15;
+var guessLeft = 13;
 var lettersGuessed = [];
-var userGuess = [];
 
-// randomly selected word variable
-var computerGuess = wordList[Math.floor(Math.random() * wordList.length)];
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+var selectedWord = randomWord;
 
 
-// functions of game
-document.onkeyup = function(event){
-    for (var i = 0; i < wordList.length; i++) {
-        currentWord [i] = "_";
+// game functions
+
+// turn random word into underscores
+function underscore() {
+    for (var i = 0; i < selectedWord.length; i++) {
+        currentWord.push("_");
     }
-    console.log(computerGuess);
+    return currentWord;
 }
+
+document.getElementById("current-word").innerHTML = underscore();
+
+console.log(underscore());
+
+console.log(randomWord);
+
+
+
+// user guesses based on key inputs
+document.onkeyup = function(event) {
+
+    // key event made to lowercase
+    var userGuess = (event.key).toLowerCase();
+    // push letters that were guess to the html
+    lettersGuessed.push(userGuess);
+    document.getElementById("letters-guessed").innerHTML = lettersGuessed;
+
+    console.log(userGuess);
+}
+
+
+
+// document.onkeyup = function(event){
+//     for (var i = 0; i < wordList.length; i++) {
+//         currentWord [i] = "_";
+//     }
+//     console.log(computerGuess);
+// }
